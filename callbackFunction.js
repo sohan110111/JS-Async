bea8528b8996700cdb541aa6ee986fc92ea7194d
sohan110111 <sohan110111@gmail.com>
2023-07-20 20:06:04 +0600

    js async

--- callbackFunction.js
+++ callbackFunction.js
@@ -6,5 +6,7 @@ function myCalculator(num1, num2, callback) {
     let sum = num1 + num2;
     callback(sum);
 }
+// to lower case
+console.log('APP DOCUMENTATION MOCKUP PHASE BUILD TASK PLAN SKETCH DESIGN'.toLowerCase());
 
 myCalculator(15, 10, myDisplay);
